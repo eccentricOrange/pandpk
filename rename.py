6637e54eb6dedf6d43bd2c4e853371e25dae6157
Eccentric Orange <eccentric.orange2@gmail.com>
2023-01-17 01:31:14 +0530

    efficiency

--- rename.py
+++ rename.py
@@ -50,12 +50,12 @@ def rename(files: Generator[Path, None, None], directory: Path) -> None:
             lesson_name = DATE_REGEX.split(file.stem)[1].strip('-_')
             reference_material = findall(r'Reference_Material_[IVX]*', file.stem)[0]
             reference_material_roman = split(r'Reference_Material_', reference_material)[1]
-            reference_material_count = fromRoman(reference_material_roman)
 
         except Exception:
             print(f"{Fore.RED}{Style.BRIGHT}[Wrong format]{Style.RESET_ALL}")
 
         else:
+            reference_material_count = fromRoman(reference_material_roman)
             new_file_name = f'{year}-{month}-{date}-{lesson_name}-{reference_material_count}{file.suffix}'
             new_file_name = new_file_name.replace('_', '-').lower()
 
